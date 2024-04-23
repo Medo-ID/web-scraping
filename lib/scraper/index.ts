694db1e059052e90820c0adae1ca7278ab746f1f
Medo-ID <midaghdour@gmail.com>
2024-04-23 16:28:52 +0100

    try to scrape amazon  product page and git title + current/original Price

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -20,8 +21,28 @@ export async function scrapeAmazonProduct(url: string) {
   };
 
   try {
+    // Fetch the product data
     const response = await axios.get(url, options);
-    console.log(response.data);
+    const $ = cheerio.load(response.data);
+
+    // Extract the product data
+    const title = $("#productTitle").text().trim();
+
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base")
+    );
+
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    console.log({ title, currentPrice, originalPrice });
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
